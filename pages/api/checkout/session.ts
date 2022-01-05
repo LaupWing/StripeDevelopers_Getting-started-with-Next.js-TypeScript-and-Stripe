4ad746dcb3953069c63878f22ff600ae4f5705d0
LaupWing <loc_strict@hotmail.com>
2022-01-05 12:18:40 +0100

    session

--- pages/api/checkout/session.ts
+++ pages/api/checkout/session.ts
@@ -5,6 +5,19 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 });
 
 
-export default (req: NextApiRequest, res: NextApiResponse) =>{
+export default async (req: NextApiRequest, res: NextApiResponse) =>{
+   const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+         {
+           // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+           price: '{{PRICE_ID}}',
+           quantity: 1,
+         },
+       ],
+       mode: 'payment',
+       success_url: `/success.html`,
+       cancel_url: `/cancel.html`,
+   })
    res.status(200).json({name: 'John Doe'})
 }
